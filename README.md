c640da291490da8695f3d97df82304dcb1b728ed
tu <tu.nguyen.lam@gmail.com>
2021-06-20 00:01:58 +0800

    remove unnessessary code

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # frontend-dash
+A data storyteller visualization for meme/outstanding events. 
 
 ## Architecture 
 We build an application that simply based on 3 layers architecture. 
 I like linear and straightforward way!!!
+
+## Inspiration 
+https://imgflip.com/ai-meme
--- src/model/configuration.py
+++ src/model/configuration.py
@@ -1,22 +1,6 @@
-from sqlalchemy import create_engine
-import pandas as pd
-from collections import defaultdict
-from copy import deepcopy
-from pyparsing import Literal, White, Word, alphanums, CharsNotIn
-from pyparsing import Forward, Group, Optional, OneOrMore, ZeroOrMore
-from pyparsing import pythonStyleComment, Empty, Combine
-from glob import iglob
-
-from urllib.parse import quote_plus as urlquote
-
-from sqlalchemy.orm import sessionmaker
-
-from src.core import injection
-
 from sqlalchemy import Column
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.types import String, JSON, SMALLINT
-
 
 
 class ConfigurationDefaultValue:
